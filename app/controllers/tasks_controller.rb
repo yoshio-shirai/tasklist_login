09a789c1f6d53338bab5517032d730316a70f87a
yoshio.shirai <yo.s1986.04@gmail.com>
2020-12-19 01:14:00 +0000

    complete_ver1

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,69 @@
+class TasksController < ApplicationController
+  before_action :require_user_logged_in, only: [:index, :show, :new, :edit]
+  before_action :correct_user, only: [:destroy]
+  
+  
+  def index
+    @tasks = Task.all
+  end
+  
+  def show
+    @task = Task.find(params[:id])
+  end
+  
+  def new
+    @task = Task.new
+  end
+  
+  def create
+    @task = current_user.tasks.build(task_params)
+    if @task.save
+      flash[:success] = 'タスクを投稿しました。'
+      redirect_to root_url
+    else
+      @tasks = current_user.tasks.order(id: :desc)
+      flash.now[:danger] = 'タスクの投稿に失敗しました。'
+      render new_task_path
+    end
+  end
+  
+  def edit
+    @task = Task.find(params[:id])
+  end
+  
+  def update
+    @task = Task.find(params[:id])
+
+    if @task.update(task_params)
+      flash[:success] = 'タスク は正常に更新されました'
+      redirect_to @task
+    else
+      flash.now[:danger] = 'タスク は更新されませんでした'
+      render :edit
+    end
+  end
+  
+
+  def destroy
+    @task.destroy
+    flash[:success] = 'タスクは正常に削除されました'
+    redirect_to root_url
+  end
+    
+
+
+  private
+
+  # Strong Parameter
+  def task_params
+    params.require(:task).permit(:content, :status)
+  end
+  
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to root_url
+    end
+  end
+  
+end
