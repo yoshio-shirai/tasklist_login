ce0d5bcc397e8c24ff8b6bbaa8d6e5a9ed4c76cb
yoshio.shirai <yo.s1986.04@gmail.com>
2020-12-20 02:43:23 +0000

    fixed v2

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,7 +1,7 @@
 class TasksController < ApplicationController
   before_action :require_user_logged_in
-  before_action :correct_user, only: [:destroy]
-  
+  before_action :correct_user, only: [ :show, :update, :edit, :destroy]
+   
   
   def index
     #@tasks = Task.all
